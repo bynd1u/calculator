02d940c9dd2a08c512526c4ffb8a330286544e37
bynd1u <semenbindu@gmail.com>
2025-01-17 12:51:27 +0100

    implement calculate function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +14,25 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function calculate(num1, operator, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2)
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      if (num2 != 0) {
+        result = divide(num1, num2)
+      }
+      break;
+  }
+  return result;
 }
